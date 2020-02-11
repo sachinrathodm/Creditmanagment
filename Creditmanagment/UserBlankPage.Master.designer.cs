e47fed365afcdfd1d6fdd628c8248f2a9d136e87
Rathod Sachin <sachinrathodm@outlook.com>
2020-02-11 08:25:56 +0530

    Set Images

--- Creditmanagment/UserBlankPage.Master.designer.cs
+++ Creditmanagment/UserBlankPage.Master.designer.cs
@@ -37,7 +37,7 @@ namespace Creditmanagment {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlImage imgUserImage_YS;
+        protected global::System.Web.UI.WebControls.Image imgUserImage_YS;
         
         /// <summary>
         /// lblName_YS control.
