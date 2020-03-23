c2acf49803a555c156ed7b36c1cf709d90fbb4e4
Rathod Sachin <sachinrathodm@outlook.com>
2020-03-23 18:17:22 +0530

    Recive Amount from the customer

--- Creditmanagment/pages/examples/Payment.aspx.designer.cs
+++ Creditmanagment/pages/examples/Payment.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Creditmanagment.pages.examples {
         protected global::System.Web.UI.WebControls.DropDownList ddCustomerName_YS;
         
         /// <summary>
+        /// lblCredit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCredit;
+        
+        /// <summary>
         /// txtPayrs_YS control.
         /// </summary>
         /// <remarks>
